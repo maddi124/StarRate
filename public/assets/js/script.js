39f802b4e01cfee45b4beb714db027587632c719
maddi124 <vmgarcia.cabrera21@gmail.com>
2022-08-03 21:55:46 -0400

    homepage slideshow img js

---
+++ public/assets/js/script.js
@@ -0,0 +1,22 @@
+var slides = document.querySelectorAll(".silder");
+var dot = document.querySelectorAll(".dots");
+var index = 0;
+
+function slideshow(){
+    if(index < 0){
+        index = slides.length-1;
+    }
+    if(index > slides.length-1){
+        index= 0;
+    }
+    for(var i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+        dots[i].classList.remove("active");
+    }
+    slides[index].style.display="block";
+    dots[index].classList.add("acttive");
+
+    index++
+
+    setTimeout(slideshow,2000);
+}
