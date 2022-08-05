7c123e06e63221bf75cbc13b21da124ec0fc7da3
maddi124 <vmgarcia.cabrera21@gmail.com>
2022-08-04 23:18:16 -0400

    homepage slideshow fixing

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,26 +1,40 @@
-var slides = document.querySelectorAll(".silder");
-var dot = document.querySelectorAll(".dots");
+var slides = document.getElementsByClassName('silder');
+var dot = document.getElementsByClassName('dot');
 var index = 0;
 
 function slideshow(){
-    if(index < 0){
-        index = slides.length-1;
+    
+    for(var i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
     }
-    if(index > slides.length-1){
-        index= 0;
+        index++;
+        console.log(slides.length);
+    if(index > slides.length) {
+        index = 1;
     }
-    for(var i=0;i<slides.length;i++){
-        slides[i].style.display="none";
-        dots[i].classList.remove("active");
+    for(var d = 0; d < dot.length; d++) {
+        dot[d].className = dot[d].className.replace('active','');
     }
-    slides[index].style.display="block";
-    dots[index].classList.add("acttive");
-
-    index++
-
-    setTimeout(slideshow,2000);
+    slides[index-1].style.display = 'block';
+    dot[index-1].className += 'active';
+    setTimeout(slideshow,2000); // change image every 2 seconds 
 }
 
+slideshow();
 
+// if(index < 0){
+//         index = slides.length-1;
+//     }
+//     if(index > slides.length-1){
+//         index= 0;
+//     }
+//     for(var i=0;i<slides.length;i++){
+//         slides[i].style.display="none";
+//         dots[i].classList.remove("active");
+//     }
+//     slides[index].style.display="block";
+//     dots[index].classList.add("acttive");
 
+//     index++
 
+//     setTimeout(slideshow,2000);
